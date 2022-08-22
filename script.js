cf78fdf1dc2cee023d8418ca5e7baf5d5293d886
Tako <takogiorgobiani97@gmail.com>
2022-08-22 21:40:22 +0400

    closure part 1

--- script.js
+++ script.js
@@ -229,3 +229,18 @@ console.log(notPrivate);
  */
 
 ///////////CLOSURES
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker(); // 1 passengers
+booker(); // 2 passengers
+booker();
